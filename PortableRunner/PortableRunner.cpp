7e245f43a0a3776283f6ada785b70530aa3d61d8
MiddleRaster <235772580+MiddleRaster@users.noreply.github.com>
2025-10-22 23:00:33 -0400

    Add files via upload

---
+++ PortableRunner/PortableRunner.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "..\tdd.h"
+
+class PortableReporter : public TDD::Reporter
+{
+	unsigned int m_testsRun, m_failedTests;
+	std::ostream& m_out;
+public:
+	PortableReporter(std::ostream& out = std::cout) : m_testsRun(0), m_failedTests(0), m_out(out) {}
+	virtual ~PortableReporter()
+	{
+		if (m_testsRun == 0)
+			m_out << "No tests were run!!!\n";
+		else {
+			m_out << m_failedTests << " failure";
+			m_out << (m_failedTests == 1 ? "" : "s");
+			m_out << " out of " << m_testsRun;
+			m_out << (m_testsRun == 1 ? " test run\n" : " tests run\n");
+		}
+	}
+	unsigned int TestsRun   () const { return m_testsRun; }
+	unsigned int TestsFailed() const { return m_failedTests; }
+
+public: // TDD::Reporter
+	virtual void ForEachTest   (const TDD::UnitTestInfo&) { ++m_testsRun; }
+	virtual void ForEachFailure(const TDD::TestFailure& tr)
+	{
+		++m_failedTests;
+
+		m_out << "Failure in " << tr.group << "." << tr.testname << " -\n";
+		m_out << tr.file_name << "(" << tr.line_number << ") : ERROR : \"" << tr.error_string << "\"\n";
+	}
+};
+
+int main()
+{
+	PortableReporter reporter;
+	TDD::Discriminator discriminator;
+	TDD::ClassRegistrarBase::RunTests(discriminator, reporter);
+	return reporter.TestsFailed();
+}
